1358336cfdb16d7e694ec0c4baac69f2cc509380
Edwin Spencer Grier <spenceg85@yahoo.com>
2021-08-03 22:27:31 -0400

    first commit :fire:

--- exercises/reverseint/index.js
+++ exercises/reverseint/index.js
@@ -8,6 +8,10 @@
 //   reverseInt(-15) === -51
 //   reverseInt(-90) === -9
 
-function reverseInt(n) {}
+function reverseInt(n) {
+    const int = n.toString();
+    let reversedInt = parseInt(int.split('').reverse().join(''), 10);
+    return Math.sign(n) === -1 ? Math.sign(n) * reversedInt : Math.sign(n) * reversedInt;
+}
 
 module.exports = reverseInt;
--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -6,6 +6,20 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-function reverse(str) {}
+function reverse(str) {
+    const reversedArray = [];
+    const stringArray = [...str];
+    stringArray.slice().reverse()
+    .forEach(item => {
+        reversedArray.push(item);
+        });
+        return reversedArray.join("");
+}
+
+// function reverse(str) {
+//     const arr = str.split('');
+//     arr.reverse();
+//     return arr.join();
+// }
 
 module.exports = reverse;
